73067888981df7497faf5a5c1b29e067f7968d88
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-08 21:31:45 -0600

    Sound Effects for gider are added, volume needs adjustin

--- Scenes/Models/Player/audio_controller.gd
+++ Scenes/Models/Player/audio_controller.gd
@@ -2,7 +2,11 @@ extends Node
 
 @export var fall_damage_player : AudioStreamPlayer
 @export var main_song : AudioStreamPlayer
+@export var sound_effects : AudioStreamPlayer
 var CorrectSound = preload("res://Assets/Sound/Music/Song 1.wav")
+var Wind = preload("res://Assets/Sound/Effects/wind.mp3")
+var is_gliding : bool = false
+@export var player : CharacterBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +15,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !main_song.is_playing():
-		main_song.stream = CorrectSound
-		main_song.play()
+	if !player.is_on_floor() and Input.is_action_just_pressed("glide"):
+		if is_gliding:
+			is_gliding = false
+			#print("glide off")
+		else:
+			if !is_gliding:
+				is_gliding = true
+				#print("glide on")
+	else:
+		if player.is_on_floor():
+			is_gliding = false
+	
+	#if !main_song.is_playing():
+		#main_song.stream = CorrectSound
+		#main_song.play()
+		
+	if is_gliding:
+		sound_effects.stream = Wind
+		sound_effects.play()
+		print(sound_effects.)
 	
 func _hurt():
 	fall_damage_player.play()
+
+func _glide():
+	pass
